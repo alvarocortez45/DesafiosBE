731f84e722f1dcf7537af04bc9018610caa553b1
Alvaro Cortez <alvarocortez455@gmail.com>
2022-04-05 23:07:04 -0300

    1ra Entrega

---
+++ 1raEntrega/app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const cartRoute = require ('./router/cartRoute')
+const productsRoute = require ('./router/productsRoute')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/api/cart', cartRoute)
+app.use('/api/products', productsRoute) 
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        error: -2 , 
+        descripcion: `Ruta: ${req.originalUrl} Metodo ${req.method} NO IMPLEMENTADO`
+    })
+})
+
+module.exports = app
